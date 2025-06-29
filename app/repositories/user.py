8a40564de99298d646a05516cdd0d3114d7ded96
PerfectSoon <totktosdox@gmail.com>
2025-06-29 16:31:51 +0300

    refactor(UserRepo): переименовать get_by_email в get_user_by_email

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -10,7 +10,7 @@ class UserRepository(BaseRepository[User]):
     def __init__(self, db: AsyncSession):
         super().__init__(db, User)
 
-    async def get_by_email(self, email: str) -> User | None:
+    async def get_user_by_email(self, email: str) -> User | None:
         try:
             res = await self.db.execute(select(User).where(User.email == email))
             return res.scalar_one_or_none()
